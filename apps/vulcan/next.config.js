d2bf303522b67dde358793edb6c6fae6bff459d5
Bo Bramer <bramer.bo@gmail.com>
2026-08-21 12:36:16 -0700

    output mode

--- apps/vulcan/next.config.js
+++ apps/vulcan/next.config.js
@@ -4,6 +4,7 @@
 const nextConfig = {
   // Next.js options go here
   // See: https://nextjs.org/docs/app/api-reference/config/next-config-js
+  output: 'export',
 };
 
 module.exports = nextConfig;
